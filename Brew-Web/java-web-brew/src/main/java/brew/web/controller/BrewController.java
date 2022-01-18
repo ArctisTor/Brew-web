bbb6ac466253555772f6b46286dce1d2b8a3988e
Chase Platz <cgplatz@scires.com>
2022-01-18 12:27:35 -0500

    Added endpoint logic.

--- Brew-Web/java-web-brew/src/main/java/brew/web/controller/BrewController.java
+++ Brew-Web/java-web-brew/src/main/java/brew/web/controller/BrewController.java
@@ -1,14 +1,17 @@
 package brew.web.controller;
 
+import brew.web.model.Brewery;
 import brew.web.service.BrewService;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
 
 @AllArgsConstructor
 @RestController
@@ -17,10 +20,10 @@ public class BrewController {
   private final BrewService brewService;
 
   @GetMapping("brew/brew/query")
-  public Gson query(String query) {
+  public ResponseEntity<?> query(String query) {
     try {
-      JsonArray jsonObject = brewService.queryBreweryDB(query);
-      return  null;
+      HashMap<String, Brewery[]> responseBody = brewService.queryBreweryDB(query);
+      return ResponseEntity.ok(responseBody);
     } catch (Exception e) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to query for Breweries");
     }
--- Brew-Web/java-web-brew/src/main/java/brew/web/service/BrewService.java
+++ Brew-Web/java-web-brew/src/main/java/brew/web/service/BrewService.java
@@ -1,15 +1,15 @@
 package brew.web.service;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.sun.deploy.net.HttpResponse;
+import brew.web.model.Brewery;
+import com.google.gson.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @Service
 public class BrewService {
@@ -24,20 +24,17 @@ public class BrewService {
     this.gson = g;
   }
 
-  public JsonArray queryBreweryDB(String query) {
+  public HashMap queryBreweryDB(String query) {
     HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
     HttpEntity httpEntity = new HttpEntity(headers);
     try {
-//       return gson.fromJson(
-//         restTemplate.getForObject(
-//           (breweryURL +"search?query="+query),  String.class
-//         ),
-//         (Type) String.class
-//       );
-//      return restTemplate.getForObject( (breweryURL +"/search?query="+query), JsonArray.class);
-      let url = (breweryURL +"/search?query="+query);
-      restTemplate.getForObject()
+      String url = (breweryURL +"/search?query="+query);
+      String responseBody =
+        restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class).getBody();
+      Brewery response[] = gson.fromJson(responseBody, Brewery[].class);
+      HashMap<String, Brewery[]> breweries = new HashMap<>();
+      breweries.put("breweryList", response);
+      return breweries;
     } catch (Exception e) {
       return null;
     }
